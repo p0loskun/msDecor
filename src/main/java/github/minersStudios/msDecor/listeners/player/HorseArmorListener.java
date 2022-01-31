0771c03800455496a9a1b34c8bc68c0273b52826
Poloskun_ <nikongricisin339@gmail.com>
2022-01-31 17:02:44 +0300

    fixed custom model data nullable issue

--- src/main/java/github/minersStudios/msDecor/listeners/player/HorseArmorListener.java
+++ src/main/java/github/minersStudios/msDecor/listeners/player/HorseArmorListener.java
@@ -1,6 +1,7 @@
 package github.minersStudios.msDecor.listeners.player;
 
 import github.minersStudios.msDecor.enums.CustomDecorMaterial;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -18,10 +19,14 @@ public class HorseArmorListener implements Listener {
                 && (event.getCursor() != null
                 && !(event.getClickedInventory() instanceof PlayerInventory)
                 && event.getCursor().getItemMeta() != null
+                && event.getCursor().getItemMeta().hasCustomModelData()
+                && event.getCursor().getType() == Material.LEATHER_HORSE_ARMOR
                 && CustomDecorMaterial.getCustomDecorMaterialByItem(event.getCursor().getItemMeta()) != null
                 || event.getClick().isShiftClick()
                 && event.getCurrentItem() != null
                 && event.getCurrentItem().getItemMeta() != null
+                && event.getCurrentItem().getItemMeta().hasCustomModelData()
+                && event.getCurrentItem().getType() == Material.LEATHER_HORSE_ARMOR
                 && CustomDecorMaterial.getCustomDecorMaterialByItem(event.getCurrentItem().getItemMeta()) != null)
         );
     }
@@ -29,9 +34,10 @@ public class HorseArmorListener implements Listener {
     @EventHandler
     public void onHorseInventoryEquip(InventoryDragEvent event){
         ItemStack itemStack = event.getNewItems().get(1);
-        if(itemStack == null) return;
+        if(itemStack == null || itemStack.getType() != Material.LEATHER_HORSE_ARMOR) return;
         event.setCancelled(
                 itemStack.getItemMeta() != null
+                && itemStack.getItemMeta().hasCustomModelData()
                 && CustomDecorMaterial.getCustomDecorMaterialByItem(itemStack.getItemMeta()) != null
                 && event.getRawSlots().contains(1)
                 && event.getInventory() instanceof HorseInventory
