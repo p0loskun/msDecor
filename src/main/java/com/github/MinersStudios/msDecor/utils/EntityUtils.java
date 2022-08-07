a081ebba09c93f5f6fb83548a2de2c678044822f
Poloskun_ <nikongricisin339@gmail.com>
2022-08-07 15:47:58 +0300

    yes, its perfectly working, thx pupsik

--- src/main/java/com/github/MinersStudios/msDecor/utils/EntityUtils.java
+++ src/main/java/com/github/MinersStudios/msDecor/utils/EntityUtils.java
@@ -6,9 +6,9 @@ import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
 
-import java.util.TreeMap;
-import java.util.NavigableMap;
 import javax.annotation.Nonnull;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 public class EntityUtils {
 
@@ -20,43 +20,17 @@ public class EntityUtils {
 	 */
 	public static void rotateArmorStandByPlayer(@Nonnull ArmorStand armorStand, @Nonnull Player player) {
 		Location armorStandLocation = armorStand.getLocation();
-		float playerRotation = player.getLocation().getYaw();
-		if (playerRotation > 25 && playerRotation < 65) {
-			armorStandLocation.setYaw(45);
-		} else if (playerRotation > 65 && playerRotation < 120) {
-			armorStandLocation.setYaw(90);
-		} else if (playerRotation > 120 && playerRotation < 140) {
-			armorStandLocation.setYaw(135);
-		} else if (playerRotation > 140 && playerRotation < 180) {
-			armorStandLocation.setYaw(180);
-		} else if (playerRotation < 25 && playerRotation > -25) {
-			armorStandLocation.setYaw(0);
-		} else if (playerRotation < -25 && playerRotation > -65) {
-			armorStandLocation.setYaw(-45);
-		} else if (playerRotation < -65 && playerRotation > -120) {
-			armorStandLocation.setYaw(-90);
-		} else if (playerRotation < -120 && playerRotation > -150) {
-			armorStandLocation.setYaw(-135);
-		} else if (playerRotation < -150 && playerRotation > -180) {
-			armorStandLocation.setYaw(-180);
+		switch (playerDirectionHandler(player)) {
+			case 1 -> armorStandLocation.setYaw(0.0f);
+			case 2 -> armorStandLocation.setYaw(45.0f);
+			case 3 -> armorStandLocation.setYaw(90.0f);
+			case 4 -> armorStandLocation.setYaw(135.0f);
+			case 5 -> armorStandLocation.setYaw(180.0f);
+			case 6 -> armorStandLocation.setYaw(-135.0f);
+			case 7 -> armorStandLocation.setYaw(-90.0f);
+			case 8 -> armorStandLocation.setYaw(-45.0f);
 		}
 		armorStand.teleport(armorStandLocation);
-	}
-
-	private static int playerDirHandler(Player player) {
-		int playerYaw = (int)player.getLocation().getYaw();
-		if (playerYaw<0) {playerYaw+=360;}
-		NavigableMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
-		map.put(0,  1);
-		map.put(23, 2);
-		map.put(68, 3);
-		map.put(113,4);
-		map.put(158,5);
-		map.put(203,6);
-		map.put(248,7);
-		map.put(293,8);
-		map.put(338,1);
-		return map.floorEntry(playerYaw).getValue();
 	}
 
 	/**
@@ -65,17 +39,32 @@ public class EntityUtils {
 	 * @param itemFrame item frame entity used for rotate item
 	 * @param player    player used for rotate item frame item
 	 */
-	public static void rotateItemFrameByPlayer(@Nonnull ItemFrame frame, @Nonnull Player player) {
-		int value = playerDirHandler(player);
-		switch (value) {
-			case 1 -> frame.setRotation(Rotation.NONE);
-			case 2 -> frame.setRotation(Rotation.CLOCKWISE_45);
-			case 3 -> frame.setRotation(Rotation.CLOCKWISE);
-			case 4 -> frame.setRotation(Rotation.CLOCKWISE_135);
-			case 5 -> frame.setRotation(Rotation.FLIPPED);
-			case 6 -> frame.setRotation(Rotation.FLIPPED_45);
-			case 7 -> frame.setRotation(Rotation.COUNTER_CLOCKWISE);
-			case 8 -> frame.setRotation(Rotation.COUNTER_CLOCKWISE_45);
+	public static void rotateItemFrameByPlayer(@Nonnull ItemFrame itemFrame, @Nonnull Player player) {
+		switch (playerDirectionHandler(player)) {
+			case 1 -> itemFrame.setRotation(Rotation.NONE);
+			case 2 -> itemFrame.setRotation(Rotation.CLOCKWISE_45);
+			case 3 -> itemFrame.setRotation(Rotation.CLOCKWISE);
+			case 4 -> itemFrame.setRotation(Rotation.CLOCKWISE_135);
+			case 5 -> itemFrame.setRotation(Rotation.FLIPPED);
+			case 6 -> itemFrame.setRotation(Rotation.FLIPPED_45);
+			case 7 -> itemFrame.setRotation(Rotation.COUNTER_CLOCKWISE);
+			case 8 -> itemFrame.setRotation(Rotation.COUNTER_CLOCKWISE_45);
 		}
+	}
+
+	private static int playerDirectionHandler(@Nonnull Player player) {
+		float playerYaw = player.getLocation().getYaw();
+		playerYaw = (playerYaw + 360) % 360;
+		NavigableMap<Float, Integer> map = new TreeMap<>();
+		map.put(0.0f,  1);
+		map.put(23.0f, 2);
+		map.put(68.0f, 3);
+		map.put(113.0f, 4);
+		map.put(158.0f, 5);
+		map.put(203.0f, 6);
+		map.put(248.0f, 7);
+		map.put(293.0f, 8);
+		map.put(338.0f, 1);
+		return map.floorEntry(playerYaw).getValue();
 	}
 }
