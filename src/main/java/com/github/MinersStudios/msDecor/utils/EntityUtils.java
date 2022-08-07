601f01bd2bff449c6af3e95b0dcb9ff759487abe
Poloskun_ <nikongricisin339@gmail.com>
2022-08-07 16:29:34 +0300

    ^-^

--- src/main/java/com/github/MinersStudios/msDecor/utils/EntityUtils.java
+++ src/main/java/com/github/MinersStudios/msDecor/utils/EntityUtils.java
@@ -21,14 +21,14 @@ public class EntityUtils {
 	public static void rotateArmorStandByPlayer(@Nonnull ArmorStand armorStand, @Nonnull Player player) {
 		Location armorStandLocation = armorStand.getLocation();
 		switch (playerDirectionHandler(player)) {
-			case 1 -> armorStandLocation.setYaw(0.0f);
-			case 2 -> armorStandLocation.setYaw(45.0f);
-			case 3 -> armorStandLocation.setYaw(90.0f);
-			case 4 -> armorStandLocation.setYaw(135.0f);
-			case 5 -> armorStandLocation.setYaw(180.0f);
-			case 6 -> armorStandLocation.setYaw(-135.0f);
-			case 7 -> armorStandLocation.setYaw(-90.0f);
-			case 8 -> armorStandLocation.setYaw(-45.0f);
+			case NONE -> armorStandLocation.setYaw(0.0f);
+			case CLOCKWISE_45 -> armorStandLocation.setYaw(45.0f);
+			case CLOCKWISE -> armorStandLocation.setYaw(90.0f);
+			case CLOCKWISE_135 -> armorStandLocation.setYaw(135.0f);
+			case FLIPPED -> armorStandLocation.setYaw(180.0f);
+			case FLIPPED_45 -> armorStandLocation.setYaw(-135.0f);
+			case COUNTER_CLOCKWISE -> armorStandLocation.setYaw(-90.0f);
+			case COUNTER_CLOCKWISE_45 -> armorStandLocation.setYaw(-45.0f);
 		}
 		armorStand.teleport(armorStandLocation);
 	}
@@ -40,31 +40,22 @@ public class EntityUtils {
 	 * @param player    player used for rotate item frame item
 	 */
 	public static void rotateItemFrameByPlayer(@Nonnull ItemFrame itemFrame, @Nonnull Player player) {
-		switch (playerDirectionHandler(player)) {
-			case 1 -> itemFrame.setRotation(Rotation.NONE);
-			case 2 -> itemFrame.setRotation(Rotation.CLOCKWISE_45);
-			case 3 -> itemFrame.setRotation(Rotation.CLOCKWISE);
-			case 4 -> itemFrame.setRotation(Rotation.CLOCKWISE_135);
-			case 5 -> itemFrame.setRotation(Rotation.FLIPPED);
-			case 6 -> itemFrame.setRotation(Rotation.FLIPPED_45);
-			case 7 -> itemFrame.setRotation(Rotation.COUNTER_CLOCKWISE);
-			case 8 -> itemFrame.setRotation(Rotation.COUNTER_CLOCKWISE_45);
-		}
+		itemFrame.setRotation(playerDirectionHandler(player));
 	}
 
-	private static int playerDirectionHandler(@Nonnull Player player) {
+	private static Rotation playerDirectionHandler(@Nonnull Player player) {
 		float playerYaw = player.getLocation().getYaw();
 		playerYaw = (playerYaw + 360) % 360;
-		NavigableMap<Float, Integer> map = new TreeMap<>();
-		map.put(0.0f,  1);
-		map.put(23.0f, 2);
-		map.put(68.0f, 3);
-		map.put(113.0f, 4);
-		map.put(158.0f, 5);
-		map.put(203.0f, 6);
-		map.put(248.0f, 7);
-		map.put(293.0f, 8);
-		map.put(338.0f, 1);
+		NavigableMap<Float, Rotation> map = new TreeMap<>();
+		map.put(0.0f,  Rotation.NONE);
+		map.put(23.0f, Rotation.CLOCKWISE_45);
+		map.put(68.0f, Rotation.CLOCKWISE);
+		map.put(113.0f, Rotation.CLOCKWISE_135);
+		map.put(158.0f, Rotation.FLIPPED);
+		map.put(203.0f, Rotation.FLIPPED_45);
+		map.put(248.0f, Rotation.COUNTER_CLOCKWISE);
+		map.put(293.0f, Rotation.COUNTER_CLOCKWISE_45);
+		map.put(338.0f, Rotation.NONE);
 		return map.floorEntry(playerYaw).getValue();
 	}
 }
